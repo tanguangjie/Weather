66e2708559c9813f5c177855a9e5d689006c88f6
tanguangjie <438994710@qq.com>
2021-08-22 16:04:46 +0800

    提交

---
+++ main.ts
@@ -0,0 +1,18 @@
+class Car { 
+    // 字段
+    engine:string;     
+    // 构造函数
+    constructor(engine:string) { 
+       this.engine = engine 
+    }      
+    // 方法
+    disp():void { 
+       console.log("函    数:"+this.engine) 
+    } 
+ } 
+  
+ // 创建一个对象
+ var obj = new Car("XXSY1")
+  
+ // 访问字段
+ console.log("读取发动机型号 :  "+obj.engine)  
